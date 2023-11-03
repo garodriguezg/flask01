883692be72a52197322dddb01b3110199d9ee473
garodriguezg <67167675+garodriguezg@users.noreply.github.com>
2023-11-03 18:47:47 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.secret_key = "mys3cr3tk3y"
+
+@app.route('/')
+def index():
+    return "hello world from Gonzalo Rodriguez"
+
+if __name__ == '__main__':
+    app.run(port=3000, host="0.0.0.0",debug=True)
