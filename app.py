05c966a598efb3f9e6616836fcdb990562f35f33
garodriguezg <67167675+garodriguezg@users.noreply.github.com>
2023-11-03 19:03:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def index():
     return "hello world from Gonzalo Rodriguez"
 
 if __name__ == '__main__':
-    app.run(port=3000, host="0.0.0.0",debug=True)
+    app.run(port=7000, host="0.0.0.0",debug=True)
